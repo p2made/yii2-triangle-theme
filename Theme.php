d3177f4fc97f92d232753b04235e61d612730509
Pedro Plowman <pedrofp@me.com>
2015-05-20 03:39:04 +1000

    Initial reorganisation

--- Theme.php
+++ Theme.php
@@ -4,11 +4,11 @@
  *
  * @copyright Copyright &copy; Pedro Plowman, https://github.com/p2made, 2015
  * @author Pedro Plowman
- * @package p2made/yii2-flat-theme
+ * @package p2made/yii2-triangle-theme
  * @license MIT
  */
 
-namespace p2made\theme\Flat;
+namespace p2made\theme\Triangle;
 
 use Yii;
 
@@ -18,7 +18,7 @@ class Theme extends \yii\base\Theme
 	 * @inheritdoc
 	 */
 	public $pathMap = [
-		'@frontend/views' => '@p2made/theme/Flat/views',
+		'@frontend/views' => '@p2made/theme/Triangle/views',
 		//'@frontend/modules' => '@p2made/theme/Flat/modules'
 	];
 }
